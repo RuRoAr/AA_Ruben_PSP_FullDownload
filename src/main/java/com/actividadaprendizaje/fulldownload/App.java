9b44c4fbb9bce5ce8bfe17ef7ad5212769d9d15d
unknown <rubenrodriguezarnedillo@gmail.com>
2021-12-20 19:35:51 +0100

    actualizacion de clases

--- src/main/java/com/actividadaprendizaje/fulldownload/App.java
+++ src/main/java/com/actividadaprendizaje/fulldownload/App.java
@@ -1,6 +1,6 @@
 package com.actividadaprendizaje.fulldownload;
 
-import com.svalero.multihilo.util.R;
+import com.svalero.multithread.util.R;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,21 +8,21 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
-
     @Override
     public void init() throws Exception {
         super.init();
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) throws Exception {//al heredar del Application te obliga a tener el metodo star
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(R.getUI("multidownload.fxml"));
-        loader.setController(new AppController());
-        ScrollPane vbox = loader.load();
+        loader.setLocation(R.getUI("fullDwnl.fxml"));//aqui carga la pricipal llama a la clase R metodo getUI
+        loader.setController(new AppController());//añade el controller
+        ScrollPane vbox = loader.load();//componente padre ScrollPane(caja grande) que donde mete la otra caja
 
         Scene scene = new Scene(vbox);
         stage.setScene(scene);
+        stage.setTitle("Downloader");
         stage.show();
     }
 
@@ -33,6 +33,5 @@ public class App extends Application {
 
     public static void main(String[] args) {
         launch();
-    }
+    }//lanza el metodo launch de Application
 }
-
--- src/main/java/com/actividadaprendizaje/fulldownload/DwnlTask.java
+++ src/main/java/com/actividadaprendizaje/fulldownload/DwnlTask.java
@@ -7,18 +7,21 @@ import org.apache.logging.log4j.Logger;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.math.RoundingMode;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.text.DecimalFormat;
 
-public class DownloadTask extends Task<Integer> {
+
+public class DwnlTask extends Task<Integer> {
 
     private URL url;
     private File file;
 
-    private static final Logger logger = LogManager.getLogger(com.svalero.multihilo.DownloadController.class);
+    private static final Logger logger = LogManager.getLogger(DwnlController.class);
 
-    public DownloadTask(String urlText, File file) throws MalformedURLException {
+    public DwnlTask(String urlText, File file) throws MalformedURLException {
         this.url = new URL(urlText);
         this.file = file;
     }
@@ -28,6 +31,8 @@ public class DownloadTask extends Task<Integer> {
         logger.trace("Descarga " + url.toString() + " iniciada");
         updateMessage("Conectando con el servidor . . .");
 
+
+        //codigo de desrcarga
         URLConnection urlConnection = url.openConnection();
         double fileSize = urlConnection.getContentLength();
         BufferedInputStream in = new BufferedInputStream(url.openStream());
@@ -37,13 +42,29 @@ public class DownloadTask extends Task<Integer> {
         int totalRead = 0;
         double downloadProgress = 0;
 
+
+
         while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
             downloadProgress = ((double) totalRead / fileSize);
+
             updateProgress(downloadProgress, 1);
+
+            DecimalFormat df = new DecimalFormat("##.##");
+            df.setRoundingMode(RoundingMode.DOWN);
+
             updateMessage(downloadProgress * 100 + " %");
 
+
+            updateMessage(totalRead/1000000 + " MB                /        " +
+                    " " + df.format(downloadProgress * 100) + " %");
             fileOutputStream.write(dataBuffer, 0, bytesRead);
             totalRead += bytesRead;
+
+            //TODO
+//            downloadByte = urlConnection.getContentLength()*downloadProgress;
+//            updateMessage(downloadByte*10024 + "bytes");
+           // downloadByte =+ 1024;
+
 
             if (isCancelled()) {
                 logger.trace("Descarga " + url.toString() + " cancelada");
@@ -51,7 +72,7 @@ public class DownloadTask extends Task<Integer> {
             }
         }
 
-        updateProgress(1, 1);
+        updateProgress(1,1);
         updateMessage("100 %");
 
         logger.trace("Descarga " + url.toString() + " finalizada");
